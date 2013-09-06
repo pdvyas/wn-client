ee107d9959a4de638ff04f05646327da80867ccc
Pratik Vyas <m@pd.io>
2013-09-06 15:43:57 +0530

    add requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import os
 
 version = '0.0.1'
 
@@ -11,5 +12,5 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=[],
+    install_requires=open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).read().split(),
 )
